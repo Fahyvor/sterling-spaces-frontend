9bb3ed58c355718cd36602c8ca4ff334baa4656b
Fahyvor <favourokafor30@gmail.com>
2024-10-29 08:05:40 +0100

    components and routing set up

---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav: React.FC = () => {
+    return (
+        <div className='bg-white backdrop-blur-sm shadow-xl p-3 px-8'>
+            <div className="name_and_logo">
+                <a href='/' className='no-underline text-black font-bold text-xl'>Sterling</a>
+            </div>
+
+            <div className='nav_links'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Nav
