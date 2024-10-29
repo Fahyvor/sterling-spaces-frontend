5f05a5acc74c791bee5b377a68b4ce550139d425
Fahyvor <favourokafor30@gmail.com>
2024-10-29 16:45:13 +0100

    auth api consumed but not tested

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,43 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../apiUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
 
 const Login: React.FC = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
+  const [isLogginIn, setIsLogginIn] = useState(false);
+  const [togglePassword, setTogglePassword] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData(prevData => ({ ...prevData, [name]: value }));
   };
 
-  const handleLogin = (e: React.FormEvent) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log("Logging in with", formData);
-    // Handle login logic here
+    try {
+      const response = await axios.post(`${API_URL}/api/auth/login`, formData);
+
+      if(response && response.status == 200) {
+       toast.success(response.data.message);
+       localStorage.setItem('userData', response.data.user);
+       localStorage.setItem('userToken', response.data.token);
+      }
+    } catch (error: any) {
+      toast.error(error)
+      console.log(error)
+    }
   };
 
   return (
     <div className="flex items-center justify-center pt-16 py-auto min-h-screen bg-gray-50 px-4">
+        <div className='toastify-message'>
+          <ToastContainer />
+        </div>
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
         <form onSubmit={handleLogin}>
@@ -32,20 +54,23 @@ const Login: React.FC = () => {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-xs font-semibold mb-2">Password</label>
+            <div className='flex items-center border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 pr-4'>
             <input
               type="password"
               name="password"
               value={formData.password}
               onChange={handleInputChange}
               required
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+              className="w-full outline-none px-4 py-2 "
             />
+            {togglePassword ? <FaRegEye onClick={() => setTogglePassword(!togglePassword)}/> : <FaRegEyeSlash onClick={() => setTogglePassword(!togglePassword)}/>} 
+            </div>
           </div>
           <button
             type="submit"
             className="w-full bg-green-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-700 transition duration-300"
           >
-            Login
+              {isLogginIn ? "Logggin In..." : "Login"}
           </button>
         </form>
         <p className="text-center text-gray-600 mt-6">
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../apiUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
 
 const Register: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -10,12 +16,15 @@ const Register: React.FC = () => {
     confirmPassword: '',
   });
 
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [isLogginIn, setIsLogginIn] = useState(false);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData(prevData => ({ ...prevData, [name]: value }));
   };
 
-  const handleRegister = (e: React.FormEvent) => {
+  const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
     if (formData.password !== formData.confirmPassword) {
       alert("Passwords do not match");
@@ -23,10 +32,23 @@ const Register: React.FC = () => {
     }
     console.log("Registering with", formData);
     // Handle registration logic here
-  };
+    try {
+      const response = await axios.post(`${API_URL}/api/auth/login`, formData);
+
+      if(response && response.data) {
+        toast(response.data.message); // Accessing message from response.data
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      toast("An error occurred during registration.");
+    }
+  }
 
   return (
     <div className="flex items-center justify-center pt-16 bg-gray-50 px-4">
+      <div className='toastify-message'>
+          <ToastContainer />
+        </div>
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold text-center mb-6">Register</h2>
         <p className='my-2 text-center'>Welcome to Sterling Spaces!</p>
@@ -81,19 +103,22 @@ const Register: React.FC = () => {
 
           <div className="mb-4">
             <label className="block text-gray-700 text-xs font-medium mb-2">Password</label>
+            <div className='flex items-center border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 pr-4'>
             <input
-              type="password"
+              type={togglePassword ? "text" : "password"}
               name="password"
               value={formData.password}
               onChange={handleInputChange}
               required
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+              className="w-full px-4 py-2 outline-none"
             />
+            {togglePassword ? <FaRegEye onClick={() => setTogglePassword(!togglePassword)}/> : <FaRegEyeSlash onClick={() => setTogglePassword(!togglePassword)}/>} 
+            </div>
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-xs font-medium mb-2">Confirm Password</label>
             <input
-              type="password"
+              type={togglePassword ? "text" : "password"}
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleInputChange}
@@ -105,7 +130,7 @@ const Register: React.FC = () => {
             type="submit"
             className="w-full bg-green-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-700 transition duration-300"
           >
-            Register
+              {isLogginIn ? "Registering..." : "Register"}
           </button>
         </form>
         <p className="text-center text-gray-600 mt-6">
